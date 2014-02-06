65d61df1dfe20461d90af592c32918c7535dd248
Jacob Leach <leach.jacob@gmail.com>
2014-02-06 18:47:29 -0500

    Fixed Javadoc

--- src/charlie/card/Hand.java
+++ src/charlie/card/Hand.java
@@ -90,8 +90,8 @@ public class Hand implements Serializable {
     }
     
     /**
-     * Tests if hand has a blackjack.
-     * @return True if hand has A+10
+     * Tests if hand is a Charlie.
+     * @return True if hand has 5 cards and the value of them is less than 21.
      */    
     public boolean isCharlie() {
         if(cards.size() == 5 && getValue() <= 21)
