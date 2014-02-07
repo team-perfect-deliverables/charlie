e5c61bcd0018e19202c680440c3d36d85af5b520
Jacob Leach <leach.jacob@gmail.com>
2014-02-07 10:55:06 -0500

    Simplified logic by making data more complex

--- src/charlie/plugins/Advisor.java
+++ src/charlie/plugins/Advisor.java
@@ -16,7 +16,8 @@ import charlie.util.Play;
  * @author jacob
  */
 public class Advisor implements IAdvisor
-{
+{   
+    //For convenience in the hard coded array
     private final Play H = Play.HIT;
     private final Play D = Play.DOUBLE_DOWN;
     private final Play S = Play.STAY;
@@ -25,13 +26,18 @@ public class Advisor implements IAdvisor
      * This will be accessed by using the value of the hand as the index for the first array.
      * The value of the dealers up card will be the index for the second array.
      *
-     * The first 9 values are blank because:
-     * A) To make the indexes match the values (there is no zero value)
-     * B) The first 8 values are the same so they are handled in an if statement for convenience
-     *
      * The first value in each sub-array is null since the dealer can not have zero for an up card 
      */
-    private final Play[][] byValue =  {{}, {}, {}, {}, {}, {}, {}, {}, {},  //9 blank values (see above comment)
+    private final Play[][] byValue =  
+                        {{},                                  //Null array to fix indexing
+                        {null, H, H, H, H, H, H, H, H, H, H}, //1
+                        {null, H, H, H, H, H, H, H, H, H, H}, //2
+                        {null, H, H, H, H, H, H, H, H, H, H}, //3
+                        {null, H, H, H, H, H, H, H, H, H, H}, //4
+                        {null, H, H, H, H, H, H, H, H, H, H}, //5
+                        {null, H, H, H, H, H, H, H, H, H, H}, //6
+                        {null, H, H, H, H, H, H, H, H, H, H}, //7
+                        {null, H, H, H, H, H, H, H, H, H, H}, //8
                         {null, H, D, D, D, D, H, H, H, H, H}, //9
                         {null, D, D, D, D, D, D, D, D, H, H}, //10
                         {null, D, D, D, D, D, D, D, D, D, H}, //11
@@ -39,7 +45,11 @@ public class Advisor implements IAdvisor
                         {null, S, S, S, S, S, H, H, H, H, H}, //13
                         {null, S, S, S, S, S, H, H, H, H, H}, //14
                         {null, S, S, S, S, S, H, H, H, H, H}, //15
-                        {null, S, S, S, S, S, H, H, H, H, H}  //16
+                        {null, S, S, S, S, S, H, H, H, H, H}, //16
+                        {null, S, S, S, S, S, S, S, S, S, S}, //17
+                        {null, S, S, S, S, S, S, S, S, S, S}, //18
+                        {null, S, S, S, S, S, S, S, S, S, S}, //19
+                        {null, S, S, S, S, S, S, S, S, S, S}  //20
                         };
 
     @Override
@@ -63,16 +73,8 @@ public class Advisor implements IAdvisor
         //There are no aces
         else 
         {
-            //If value is less than 8, we always hit
-            if(literalValue <= 8)
-            {
-                toReturn = Play.HIT;
-            }
-            else
-            {
-                
-            }
-        }
+            
+        } 
 
         return toReturn;
     }
