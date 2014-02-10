39873797678d76bed889cd99c7e62765a8e731bb
Jacob Leach <leach.jacob@gmail.com>
2014-02-10 11:16:06 -0500

    Randomgeneratortest finished

--- test/charlie/test/RandomCardGeneratorTest.java
+++ test/charlie/test/RandomCardGeneratorTest.java
@@ -1,6 +1,7 @@
 package charlie.test;
 
 import charlie.card.Card;
+import java.util.ArrayList;
 import java.util.HashMap;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
@@ -11,32 +12,36 @@ import org.junit.Test;
  */
 public class RandomCardGeneratorTest
 {
-    private static final int RUN_NUMBER = 10000;
-    
+
+    private static final int RUN_NUMBER = 100000;
+
+    //This should return true, most of the time. It's random so it could not sometimes.
     @Test
     public void generatorRandomnessTest()
     {
         HashMap<Card, Integer> randomness = new HashMap<>();
-        for(int i = 0; i < RUN_NUMBER; i++)
+        ArrayList<Card> cards = new ArrayList<>();
+
+        for (int i = 0; i < RUN_NUMBER; i++)
         {
             Card randomCard = RandomCardGenerator.getRandomCard();
-            if(randomness.containsKey(randomCard))
+
+            if (!(randomness.containsKey(randomCard)))
+            {
+                randomness.put(randomCard, 1);
+                cards.add(randomCard);
+            } else
             {
                 randomness.put(randomCard, randomness.get(randomCard) + 1);
             }
-            else
-            {
-                randomness.put(RandomCardGenerator.getRandomCard(), 1);
-            }
         }
-        
-        Object[] cards = randomness.keySet().toArray();
-        
-        for(Object o : cards)
+
+        Card first = RandomCardGenerator.getRandomCard();
+        System.out.println(randomness.get(first));
+        for (Card i : cards)
         {
-            System.out.println("Card: " + (Card) o + " - " + randomness.get((Card) o));
+            System.out.println((randomness.get(first) / 1.2) + " < " + randomness.get(i) + " < " + (randomness.get(first) * 1.2));
+            assertTrue((randomness.get(first) / 1.2) < randomness.get(i) && randomness.get(i) < (randomness.get(first) * 1.2));
         }
-        
-        assertTrue(true);
     }
 }
